25c2d973bd9d0901835a8c983015df218f5ad050
GREENBANYAN <greenbanyan@163.com>
2017-11-12 22:31:32 +0800

    skoal

--- skoal/skoal.h
+++ skoal/skoal.h
@@ -1,13 +1,54 @@
+// skoal.h
 //
-//  skoal.h
-//  skoal-example
+// Copyright (c) 2017 BANYAN
 //
-//  Created by BANYAN on 2017/11/12.
-//  Copyright © 2017年 skoal. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <HealthKit/HealthKit.h>
+
+///-------------------------
+#pragma mark - DEBUG
+///-------------------------
+#ifdef DEBUG
+#define DLog(FORMAT, ...) fprintf(stderr, "%s [Line %zd]\t%s\n", [[[NSString stringWithUTF8String: __FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat: FORMAT, ## __VA_ARGS__] UTF8String]);
+#else
+#define DLog(FORMAT, ...) nil
+#endif
+
+typedef NS_ENUM(NSUInteger,HealthyStorePermissionResponse) {
+    HealthyStorePermissionResponseError = 0,
+    HealthyStorePermissionResponseSuccess
+};
+
+typedef void (^HealthyStorePermissionResponseBlock)(HealthyStorePermissionResponse permissionResponse);
 
 @interface skoal : NSObject
+
++(instancetype)sharedInstance;
+
+@property (nonatomic,copy) HealthyStorePermissionResponseBlock permissionResponseBlock;
+
+/**!
+ * @brief 获取HealthyKit权限
+ */
+-(void)requestHealthyPermissionWithBlock:(HealthyStorePermissionResponseBlock)block;
 
 @end
--- skoal/skoal.m
+++ skoal/skoal.m
@@ -1,13 +1,67 @@
+// skoal.m
 //
-//  skoal.m
-//  skoal-example
+// Copyright (c) 2017 BANYAN
 //
-//  Created by BANYAN on 2017/11/12.
-//  Copyright © 2017年 skoal. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "skoal.h"
 
+static skoal *_instance = nil;
+
 @implementation skoal
+
++(instancetype)sharedInstance{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _instance = [[self alloc] init];
+    });
+    return _instance;
+}
++(instancetype)allocWithZone:(struct _NSZone *)zone{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _instance = [super allocWithZone:zone];
+    });
+    return _instance;
+}
++(id)copyWithZone:(nullable NSZone *)zone{
+    return _instance;
+}
+
+#pragma mark - 获取HealthyKit权限
+-(void)requestHealthyPermissionWithBlock:(HealthyStorePermissionResponseBlock)block
+{
+    if (![HKHealthStore isHealthDataAvailable]) {
+        DLog(@"HealthyKit:该设备不支持HealthyKit");
+    }else{
+        HKHealthStore *store = [[HKHealthStore alloc] init];
+        NSSet *readObjectTypes = [NSSet setWithObjects:[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount],
+                                  [HKObjectType quantityTypeForIdentifier:
+                                   HKQuantityTypeIdentifierDistanceWalkingRunning],[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMassIndex],[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate],nil];
+        [store requestAuthorizationToShareTypes:nil readTypes:readObjectTypes completion:^(BOOL success, NSError * _Nullable error) {
+            if (success == YES) {
+                block(HealthyStorePermissionResponseSuccess);
+            }else{
+                block(HealthyStorePermissionResponseError);
+            }
+        }];
+    }
+}
 
 @end
