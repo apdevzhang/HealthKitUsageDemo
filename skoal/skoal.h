21ba57244daac9129800fbeac6969b19f8ec1fe8
GREENBANYAN <greenbanyan@163.com>
2017-11-12 18:51:01 +0800

    skoal

---
+++ skoal/skoal.h
@@ -0,0 +1,13 @@
+//
+//  skoal.h
+//  skoal-example
+//
+//  Created by BANYAN on 2017/11/12.
+//  Copyright © 2017年 skoal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface skoal : NSObject
+
+@end
---
+++ skoal/skoal.m
@@ -0,0 +1,13 @@
+//
+//  skoal.m
+//  skoal-example
+//
+//  Created by BANYAN on 2017/11/12.
+//  Copyright © 2017年 skoal. All rights reserved.
+//
+
+#import "skoal.h"
+
+@implementation skoal
+
+@end
